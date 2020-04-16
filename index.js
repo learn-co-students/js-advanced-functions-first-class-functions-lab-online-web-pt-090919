52c00cc9e6a4f2918d349dce9a6eecb18c509581
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-16 01:28:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function (drivers) {
-  let newArray = []
-  return newArray.push(drivers[0], drivers[1])
+  return drivers.slice(0, 2)
 }
-const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+let drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
 returnFirstTwoDrivers(drivers)
+
+const returnLastTwoDrivers = function(drivers) {
+  return drivers.slice(-2)
+}
+returnLastTwoDrivers(drivers)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (a){
+  return function(b) {
+    return a * b; 
+  }
+}
+createFareMultiplier(2)(3)
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(drivers, whichDrivers){
+  return whichDrivers(drivers);
+}
 
 
